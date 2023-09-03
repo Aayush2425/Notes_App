a5b59e1313f95723b82aa10849762ce2af0cb56d
Aayush2425 <122793682+Aayush2425@users.noreply.github.com>
2023-09-03 13:13:00 +0530

    changes made

--- Notes/src/Components/Header.jsx
+++ Notes/src/Components/Header.jsx
@@ -1,6 +1,6 @@
 export default function Header() {
   return (
-    <div className=" pt-8 flex justify-center">
+    <div className=" pt-8 flex justify-center md:flex md:justify-center w-full">
     <h1 className='text-3xl font-bold'>MyNotesApp</h1>
     </div>
   )
