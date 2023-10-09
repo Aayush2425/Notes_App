3ae08f157b74a59d87f7b4680e079dca61ef2b73
Aayush2425 <122793682+Aayush2425@users.noreply.github.com>
2023-10-09 23:18:11 +0530

    Helloo

--- Notes/vite.config.js
+++ Notes/vite.config.js
@@ -3,14 +3,5 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Notes_App/",
   plugins: [react()],
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://localhost:4000",
-        secure: false,
-      },
-    },
-  },
 });
